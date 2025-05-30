5e7aee0dc2a019ca36c1ca0f67b42fee87f30c87
prayag78 <dasprayag1234@gmail.com>
2025-05-30 18:41:22 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/ModeToggle";
+
+const page = () => {
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+};
+
+export default page;
